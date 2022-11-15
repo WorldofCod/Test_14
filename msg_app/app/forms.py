827cbf36b50b7466f648f164d1da23e8324f40ad
WorldofCod <patilmmanjusha@gmail.com>
2022-11-15 07:58:33 +0530

    new msg_app

---
+++ msg_app/app/forms.py
@@ -0,0 +1,26 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm,AuthenticationForm
+from django.contrib.auth.models import User
+
+
+# Create your forms here.
+
+class NewUserForm(UserCreationForm):
+	email = forms.EmailField(required=True)
+
+	class Meta:
+		model = User
+		fields = ("username", "email", "password1", "password2")
+
+	def save(self, commit=True):
+		user = super(NewUserForm, self).save(commit=False)
+		user.email = self.cleaned_data['email']
+		if commit:
+			user.save()
+		return user
+
+class ContactForm(forms.Form):
+    first_name = forms.CharField(max_length=50)
+    last_name = forms.CharField(max_length=50)
+    email_address = forms.EmailField(max_length=100)
+    message = forms.CharField(widget = forms.Textarea, max_length = 2000)
